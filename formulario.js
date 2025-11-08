c4185a3ed1550468e2277e3077dd9bdd8b41e331
fcali17 <fraancali17@gmail.com>
2025-11-08 15:38:03 -0300

    mensaje1

---
+++ formulario.js
@@ -0,0 +1,11 @@
+//crtea un array
+console.log("Funcionando")
+
+
+const boton = document.querySelector(".cart")
+
+
+boton.addEventListener("click", (evento)=>{
+    evento.preventDefault();
+    alert("BOTONNNN")
+})
