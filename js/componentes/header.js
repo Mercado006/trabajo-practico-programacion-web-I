590e23c8a0c3bc1d65a68646d79fcfab6eae2cfc
May <mayraleonardi@gmail.com>
2025-11-11 20:22:43 -0300

    Se agrego el renderizado header y footer en el index. Tambien se agrego el sidebar al componentes header.js

--- js/componentes/header.js
+++ js/componentes/header.js
@@ -25,12 +25,39 @@ export const headerHTML = `
             <li><a href="/pages/PaginaContacto.html">Contacto</a></li>
         </ul>
 
-        <div class="nav-actions">
-            <a href="/pages/login.html" class="login">Acceder</a>
-            <a href="/pages/formulario-inscripcion.html" class="cart">
-                <img src="/img/carrito.png" alt="Carrito de cursos">
-                <span class="cart-count">0</span>
-            </a>
+         <div class="nav-actions">
+        <a href="./pages/login.html" class="login">Acceder</a>
+
+        <button id="cart-button" class="cart">
+          <img src="./img/carrito.png" alt="Carrito de cursos">
+          <span class="cart-count">0</span>
+        </button>
+
+        <!-- Sidebar del carrito -->
+        <div id="cart-sidebar" class="cart-sidebar">
+          <div id="info-sidebar" class="info-sidebar">
+            <h2>Mi carrito</h2>
+            <div id="cart-count-info" class="cart-count-info">
+              <span class="cart-count-sidebar">0</span>
+              <p> unidades</p>
+            </div>
+          </div>
+          <table id="side-bar" class="side-bar">
+            <thead>
+              <tr>
+                <th>Imagen</th>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Cantidad</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody id="cart-items"></tbody>
+          </table>
+          <div id="sidebar-button" class="sidebar-button">
+            <button id="delete-cart-count" class="delete-cart-count">Vaciar Carrito</button>
+            <button id="close-cart" class="close-cart">Cerrar</button>
+          </div>
         </div>
     </nav>
 </header>
